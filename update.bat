f3dfc8d20a37ecae7db10084102b273fd86b155e
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2019-02-05 17:11:42 +0100

    Fix updater

--- update.bat
+++ update.bat
@@ -1,9 +1,9 @@
 @echo off
 echo Updating...
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/SkydeFR/SalienSkScript/master/README.md -OutFile README.md"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/SkydeFR/SalienSkScript/master/.dockerignore -OutFile .dockerignore"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/SkydeFR/SalienSkScript/master/.gitattributes -OutFile .gitattributes"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/SkydeFR/SalienSkScript/master/.gitignore -OutFile .gitignore"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/SkydeFR/SalienSkScript/master/Dockerfile -OutFile Dockerfile"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/SkydeFR/SalienSkScript/master/LICENSE -OutFile LICENSE"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/SkydeFR/SalienSkScript/master/README.md -OutFile README.md"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/SkydeFR/SalienSkScript/master/Vagrantfile -OutFile Vagrantfile"
@@ -11,9 +11,9 @@ powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/SkydeFR
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/SkydeFR/SalienSkScript/master/cheat.bat -OutFile cheat.bat"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/SkydeFR/SalienSkScript/master/cheat.php -OutFile cheat.php"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/SkydeFR/SalienSkScript/master/cheat.py -OutFile cheat.py"
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/SkydeFR/SalienSkScript/master/Dockerfile -OutFile Dockerfile"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/SkydeFR/SalienSkScript/master/downloadphp.ps1 -OutFile downloadphp.ps1"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/SkydeFR/SalienSkScript/master/downloadpython.ps1 -OutFile downloadpython.ps1"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/SkydeFR/SalienSkScript/master/python-cheat.bat -OutFile python-cheat.bat"
-if not errorlevel 1 echo Update done!
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/SkydeFR/SalienSkScript/master/update.bat -OutFile update.bat"
+echo Update done!
 pause
