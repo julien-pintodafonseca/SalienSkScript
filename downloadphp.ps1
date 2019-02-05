cfae337d33908b23c660f7559b6d1d2002e0ead6
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2019-02-05 17:11:35 +0100

    Added account name retriever for CLI

--- downloadphp.ps1
+++ downloadphp.ps1
@@ -30,3 +30,4 @@ else
 }
 Copy-Item -Path php\php.ini-production -Destination php\php.ini
 ((Get-Content php\php.ini)) -Replace ";extension=curl", ("extension=" + (Get-Item -Path ".\php") + "\ext\php_curl.dll") | Set-Content php\php.ini
+((Get-Content php\php.ini)) -Replace ";extension=openssl", ("extension=" + (Get-Item -Path ".\php") + "\ext\php_openssl.dll") | Set-Content php\php.ini
